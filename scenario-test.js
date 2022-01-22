e118a05719a5e6547d554473b88a30c1f5ad0dc4
Atieh.Baratinia <atiehbaratinia@gmail.com>
2022-01-23 00:41:59 +0330

    homework finished

--- scenario-test.js
+++ scenario-test.js
@@ -1,29 +1,83 @@
+
+import { check} from 'k6';
+import {randomItem } from "https://jslib.k6.io/k6-utils/1.1.0/index.js";
+import { Httpx} from 'https://jslib.k6.io/httpx/0.0.2/index.js';
 import http from 'k6/http';
-import { check, group, sleep } from 'k6';
 
-const BASE_URL = 'https://test-api.k6.io';
+let session = new Httpx({baseURL: 'https://test-api.k6.io'});
 
+const USERNAME = 'hanieeeeeh';
+const PASSWORD = 'hanieh';
 export const options = {
-    stages: [
-        { duration: '10s', target: 1000 }, // simulate ramp-up of traffic from 1 to 100 users over 5 seconds.
-        { duration: '10s', target: 1000 }, // stay at 100 users for 10 seconds
-        { duration: '5s', target: 0 }, // ramp-down to 0 users
-      ],
+    
     thresholds: {
-      http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-      http_req_duration: ['p(95)<400'], // 95% of requests should be below 200ms
+        http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+        http_req_duration: ['p(90)<800'], // 95% of requests should be below 200ms
     },
+    scenarios:{
+        
+        scenario1:{
+            executor: 'shared-iterations',
+            exec: "func1",
+            vus: 100,
+            iterations: 100,
+            maxDuration: '10s',
+            
+        }, 
+        scenario2:{
+            vus: 100,
+            maxDuration: '10s',
+            executor: 'shared-iterations',
+            exec: "func2",
+            iterations: 100,
+            
+        }
+    }
+    
+};
+
+
+
+export function func2() {
+    
+    const loginRes = session.post(`/auth/token/login/`, {
+      username: USERNAME,
+      password: PASSWORD,
+    });
+  
+    check(loginRes, {
+      'logged in successfully': (resp) => resp.json('access') !== '',
+    });
+
+    let authToken = loginRes.json('access');
+    session.addHeader('Authorization', `Bearer ${authToken}`);
+    
+
+    const myObjects = session.get(`/my/crocodiles/`).json();
+    check(myObjects, { 'retrieved crocodiles': (obj) => obj.length > 0 });
+    
+    if(myObjects.length < 10){
+        let croc = {
+            name: `Crocy ${i}`,
+            sex: randomItem(["M", "F"]),
+            date_of_birth: '2020-01-01',
+        };
+        let insert = session.post(`/my/crocodiles/`, croc);
+        check(insert, {'is status 201': (obj) => obj.status ===201,});
+    }
+    
   };
 
-export default function() {
-
-  const myObjects = http.get(`${BASE_URL}/public/crocodiles/`).json();
-  const length = myObjects.length
-  const rand = Math.floor((Math.random() * length));
-  const cro_id = myObjects[rand]["id"];
   
-  const obj1 = http.get(`${BASE_URL}/public/crocodiles/${cro_id}/`);
+const BASE_URL = 'https://test-api.k6.io';
+export function func1(){
+    const myObjects = http.get(`${BASE_URL}/public/crocodiles/`).json();
+    const length = myObjects.length
+    const rand = Math.floor((Math.random() * length));
+    const cro_id = myObjects[rand]["id"];
+    
+    const obj1 = http.get(`${BASE_URL}/public/crocodiles/${cro_id}/`);
+    
+    check(obj1, { 'is status 200': (obj) => obj.status ===200, });
   
-  check(obj1, { 'is status 200': (obj) => obj.status ===200, });
-
 };

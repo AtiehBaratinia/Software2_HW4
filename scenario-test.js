73d6295a5cd1e2f4c13ed3057c91b21ed7dec436
Atieh.Baratinia <atiehbaratinia@gmail.com>
2022-01-22 21:01:01 +0330

    added the second scenario

--- scenario-test.js
+++ scenario-test.js
@@ -1,16 +1,19 @@
 import http from 'k6/http';
 import { check, group, sleep } from 'k6';
 
-export const options = {
-  stages: [
-    { duration: '5s', target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
-    { duration: '10s', target: 100 }, // stay at 100 users for 10 minutes
-    { duration: '5s', target: 0 }, // ramp-down to 0 users
-  ]
-};
-
 const BASE_URL = 'https://test-api.k6.io';
 
+export const options = {
+    stages: [
+        { duration: '10s', target: 1000 }, // simulate ramp-up of traffic from 1 to 100 users over 5 seconds.
+        { duration: '10s', target: 1000 }, // stay at 100 users for 10 seconds
+        { duration: '5s', target: 0 }, // ramp-down to 0 users
+      ],
+    thresholds: {
+      http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+      http_req_duration: ['p(95)<400'], // 95% of requests should be below 200ms
+    },
+  };
 
 export default function() {
 
